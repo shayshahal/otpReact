2097fb9227ca8dc93d1ced7ff60f93eb4834143c
shayshahal <shay840@gmail.com>
2023-06-13 10:31:59 +0300

    finish server

--- server/app.js
+++ server/app.js
@@ -1,41 +1,80 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const asyncHandler = require('express-async-handler');
+const Twilio = require('twilio');
+dotenv.config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSid = process.env.TWILIO_SERVICE_SID;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const client = new Twilio(accountSid, authToken);
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((req, res, next) => {
+	req.body.phoneNumber = '+972' + req.body.phoneNumber;
+	next();
+});
+
+app.post(
+	'/send-verification-code',
+	asyncHandler(async (req, res) => {
+		const phoneNumber = req.body.phoneNumber;
+		await client.verify.v2
+			.services(serviceSid)
+			.verifications.create({ to: phoneNumber, channel: 'sms' });
+		console.log('Success!');
+		res.status(200).send('success');
+	})
+);
+
+app.post(
+	'/verify-code',
+	asyncHandler(async (req, res) => {
+		const { phoneNumber, verificationCode } = req.body;
+
+		const verificationCheck = await client.verify.v2
+			.services(serviceSid)
+			.verificationChecks.create({
+				to: phoneNumber,
+				code: verificationCode,
+			});
+
+		if (verificationCheck.status === 'approved') {
+			res.sendStatus(200);
+			console.log('Success!');
+		}
+	})
+);
+
+app.get('/', function (req, res) {
+  res.send('lol')
+})
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
