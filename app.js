c54cfc112721edb6d807bc424167d609ce9c8529
shayshahal <shay840@gmail.com>
2023-06-13 12:53:32 +0300

    changes for deployment

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(express.static(__dirname));
+app.use(express.static('/client/dist'));
 
 app.use((req, res, next) => {
 	req.body.phoneNumber = '+972' + req.body.phoneNumber;
